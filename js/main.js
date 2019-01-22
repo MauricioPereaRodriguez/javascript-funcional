4717cb9fa4e8d9cb66c9d810a9035c1273acc6ad
Mauricio Perea Rodríguez <mperea@portalteck.com>
2019-01-22 10:36:12 -0500

    Se agregan validación de los inputs

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 const compose = (...functions) => data =>
-  functions.reduceRight((value, func) => func(value), data)
+    functions.reduceRight((value, func) => func(value), data)
+
+$description.onkeypress = () => {
+    $description.classList.remove('is-invalid')
+}
+
+$calories.onkeypress = () => {
+    $calories.classList.remove('is-invalid')
+}
+
+$carbs.onkeypress = () => {
+    $carbs.classList.remove('is-invalid')
+}
+
+$protein.onkeypress = () => {
+    $protein.classList.remove('is-invalid')
+}
+
+const validateInputs = () => {
+
+    ($description.value === '') ? $description.classList.add('is-invalid'): '';
+    ($calories.value === '') ? $calories.classList.add('is-invalid'): '';
+    ($carbs.value === '') ? $carbs.classList.add('is-invalid'): '';
+    ($protein.value === '') ? $protein.classList.add('is-invalid'): '';
+
+}
