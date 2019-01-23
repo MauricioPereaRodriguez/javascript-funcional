b35e5a38fc5551070206f0ce98caea26c507444e
Mauricio Perea Rodríguez <mperea@portalteck.com>
2019-01-22 20:32:57 -0500

    Se agregan métodos para agregar un ítem a la lista y para limpiar los inputs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 const compose = (...functions) => data =>
     functions.reduceRight((value, func) => func(value), data)
 
+
+let list = [
+    /*{
+        description: 'Manzana',
+        calories: 10,
+        carbs: 10,
+        protein:10
+    },
+    {
+        description: 'Manzana',
+        calories: 10,
+        carbs: 10,
+        protein:10
+    },
+    {
+        description: 'Manzana',
+        calories: 10,
+        carbs: 10,
+        protein:10
+    },
+    {
+        description: 'Manzana',
+        calories: 10,
+        carbs: 10,
+        protein:10
+    }*/
+];
 $description.onkeypress = () => {
     $description.classList.remove('is-invalid')
 }
@@ -19,9 +46,34 @@ $protein.onkeypress = () => {
 
 const validateInputs = () => {
 
-    ($description.value === '') ? $description.classList.add('is-invalid'): '';
-    ($calories.value === '') ? $calories.classList.add('is-invalid'): '';
-    ($carbs.value === '') ? $carbs.classList.add('is-invalid'): '';
-    ($protein.value === '') ? $protein.classList.add('is-invalid'): '';
+  ($description.value === '') ? $description.classList.add('is-invalid'): '';
+  ($calories.value === '') ? $calories.classList.add('is-invalid'): '';
+  ($carbs.value === '') ? $carbs.classList.add('is-invalid'): '';
+  ($protein.value === '') ? $protein.classList.add('is-invalid'): '';
 
+  if ($description.value && $calories.value && $carbs.value && $protein.value)
+    add()
+
+}
+
+const add = () => {
+
+  const newItem = {
+    description:  $description.value,
+    calories:     parseInt($calories.value),
+    carbs:        parseInt($carbs.value),
+    protein:      parseInt($protein.value),
+  }
+
+  list.push(newItem)
+  cleanInputs()
+  console.log(list)
+
+}
+
+const cleanInputs = () => {
+  $description.value  = ''
+  $calories.value     = ''
+  $carbs.value        = ''
+  $protein.value      = ''
 }
